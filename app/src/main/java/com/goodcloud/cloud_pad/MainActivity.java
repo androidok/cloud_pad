6300ec4aebf24ca71705e5e53178176c6b6e6c34
binglin881211 <binglin881211@163.com>
2019-02-12 20:50:26 +0800

    注释测试

--- app/src/main/java/com/goodcloud/cloud_pad/MainActivity.java
+++ app/src/main/java/com/goodcloud/cloud_pad/MainActivity.java
@@ -3,6 +3,9 @@ package com.goodcloud.cloud_pad;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 好用云智慧工厂系统移动端（安卓APP）企业平板端APP
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
